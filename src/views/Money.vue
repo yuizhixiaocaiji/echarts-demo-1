58eea30388399355f1f469384e60934ebfff3afb
你çxuCheng <1390683299@qq.com>
2021-09-03 20:27:10 +0800

    money 页面布局

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -1,6 +1,6 @@
 <template >
-  <div >
-    Money
+  <div class="nav-wrapper" >
+    <div class="content" >Money</div >
     <Nav />
   </div >
 </template >
@@ -12,5 +12,16 @@ export default {
 </script >
 
 <style lang="scss" scoped >
+.nav-wrapper {
+  border: 1px solid green;
+  display: flex;
+  flex-direction: column;
+  height: 100vh;
+}
 
+.content {
+  border: 1px solid blue;
+  overflow: auto;
+  flex-grow: 1;
+}
 </style >
