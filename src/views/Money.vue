35ac9a4d2b68db73678fb2b317d8ba6fc52aafae
你çxuCheng <1390683299@qq.com>
2021-09-04 19:41:39 +0800

    可以选中和取消tag

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -1,26 +1,32 @@
-<template>
-  <Layout class-prefix="layout">
-    <NumberPad/>
+<template >
+  <Layout class-prefix="layout" >
+    <NumberPad />
     <Types />
-    <Notes/>
-    <Tags/>
-  </Layout>
-</template>
+    <Notes />
+    <Tags :data-source="tags" />
+  </Layout >
+</template >
 
-<script lang="ts">
+<script >
 import NumberPad from '@/components/Money/NumberPad.vue';
 import Types from '@/components/Money/Types.vue';
 import Notes from '@/components/Money/Notes.vue';
 import Tags from '@/components/Money/Tags.vue';
+
 export default {
   name: 'Money',
-  components: {Tags, Notes, Types, NumberPad},
+  components: { Tags, Notes, Types, NumberPad },
+  data() {
+    return {
+      tags: [ '衣', '食', '住', '行' ]
+    }
+  }
 };
-</script>
+</script >
 
-<style lang="scss">
+<style lang="scss" >
 .layout-content {
   display: flex;
   flex-direction: column-reverse;
 }
-</style>
+</style >
