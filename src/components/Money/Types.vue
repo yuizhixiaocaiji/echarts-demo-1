1ecb660941c2218e787bb0f4422eee508f284725
你çxuCheng <1390683299@qq.com>
2021-09-04 15:09:29 +0800

    使用js实现了一个组件

--- src/components/Money/Types.vue
+++ src/components/Money/Types.vue
@@ -1,24 +1,46 @@
-<template>
-  <div>
-    <ul class="types">
-      <li class="selected">支出</li>
-      <li>收入</li>
-    </ul>
-  </div>
-</template>
+<template >
+  <div >
+    <ul class="types" >
+      <li :class="type === '-' && 'selected' "
+          @click="selectType('-')" >支出
+      </li >
+      <li :class="type === '+' && 'selected' "
+          @click="selectType('+')" >收入
+      </li >
+    </ul >
+  </div >
+</template >
 
-<script lang="ts">
+<script >
 export default {
-  name: 'Types'
+  name: 'Types',
+  props: [ 'xxx' ],
+  data() {
+    return {
+      type: '-' //'-'表示支出，'+'表示收入
+    }
+  },
+  mounted() {
+    console.log(this.xxx)
+  },
+  methods: {
+    selectType(type) { //type只能是加号减号之间的一个
+      if (type !== '-' && type !== '+') {
+        throw new Error('type is unknown')
+      }
+      this.type = type;
+    }
+  }
 };
-</script>
+</script >
 
-<style lang="scss" scoped>
+<style lang="scss" scoped >
 .types {
   background: #c4c4c4;
   display: flex;
   text-align: center;
   font-size: 24px;
+
   > li {
     width: 50%;
     height: 64px;
@@ -26,6 +48,7 @@ export default {
     justify-content: center;
     align-items: center;
     position: relative;
+
     &.selected::after {
       content: '';
       position: absolute;
@@ -37,4 +60,4 @@ export default {
     }
   }
 }
-</style>
+</style >
