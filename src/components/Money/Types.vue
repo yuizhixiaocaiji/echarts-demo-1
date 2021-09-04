06db5e265dbea4669e152d4594a13525657517f7
你çxuCheng <1390683299@qq.com>
2021-09-04 16:43:57 +0800

    完成Types组件

--- src/components/Money/Types.vue
+++ src/components/Money/Types.vue
@@ -11,27 +11,20 @@
   </div >
 </template >
 
-<script >
-export default {
-  name: 'Types',
-  props: [ 'xxx' ],
-  data() {
-    return {
-      type: '-' //'-'表示支出，'+'表示收入
+<script lang="ts" >
+import Vue from 'vue';
+import {Component, Prop} from 'vue-property-decorator';
+
+@Component
+export default class Types extends Vue {
+  type = '-'; //'-'表示支出，'+'表示收入
+  selectType(type: string) {
+    if (type !== '-' && type !== '+') {
+      throw new Error('type is unknown');
     }
-  },
-  mounted() {
-    console.log(this.xxx)
-  },
-  methods: {
-    selectType(type) { //type只能是加号减号之间的一个
-      if (type !== '-' && type !== '+') {
-        throw new Error('type is unknown')
-      }
-      this.type = type;
-    }
+    this.type = type;
   }
-};
+}
 </script >
 
 <style lang="scss" scoped >
