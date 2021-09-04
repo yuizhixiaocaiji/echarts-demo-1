d812df9241b01c746e2924479683f7fc9ed67425
你çxuCheng <1390683299@qq.com>
2021-09-04 20:13:50 +0800

    完成了 tag 的增加

--- src/components/Money/Tags.vue
+++ src/components/Money/Tags.vue
@@ -1,7 +1,7 @@
 <template >
   <div class="tags" >
     <div class="new" >
-      <button >新增标签</button >
+      <button @click="create" >新增标签</button >
     </div >
     <ul class="current" >
       <li v-for="tag in dataSource" :key="tag"
@@ -19,7 +19,7 @@ import {Component, Prop} from 'vue-property-decorator';
 
 @Component
 export default class Tags extends Vue {
-  @Prop() dataSource: string[] | undefined;
+  @Prop() readonly dataSource: string[] | undefined;
   selectedTags: string[] = [];
 
   toggle(tag: string) {
@@ -28,6 +28,16 @@ export default class Tags extends Vue {
       this.selectedTags.splice(index, 1);
     } else {
       this.selectedTags.push(tag);
+    }
+  }
+
+  create() {
+    const name = window.prompt('请输入标签名');
+    if (name === '') {
+      window.alert('标签名不能为空');
+    } else if (this.dataSource) {
+      this.$emit('update:dataSource',
+          [...this.dataSource, name]);
     }
   }
 }
