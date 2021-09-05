e88774bfe023c05080fbbffe0947466754623066
你çxuCheng <1390683299@qq.com>
2021-09-05 14:59:50 +0800

    将tag的string改为id和name

---
+++ src/views/EditLabel.vue
@@ -0,0 +1,19 @@
+<template >
+  <layout >
+    编辑标签
+  </layout >
+</template >
+
+<script lang="ts" >
+import Vue from 'vue';
+import {Component} from 'vue-property-decorator';
+
+@Component
+export default class EditLabel extends Vue {
+
+}
+</script >
+
+<style lang="scss" scoped >
+
+</style >
