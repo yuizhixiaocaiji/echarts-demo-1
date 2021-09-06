69187048d8ebf111f9688db59e20a6be4216de50
你çxuCheng <1390683299@qq.com>
2021-09-06 11:50:14 +0800

    Money.vue 使用 vuex

--- src/views/EditLabel.vue
+++ src/views/EditLabel.vue
@@ -3,7 +3,7 @@
     <div class="navBar" >
       <Icon class="leftIcon" name="left" @click="goBack" />
       <span class="title" >编辑标签</span >
-      <span class="rightIcon" ></span >
+      <span class="rightIcon" />
     </div >
     <div class="form-wrapper" >
       <FormItem :value="tag.name"
@@ -22,13 +22,14 @@ import Vue from 'vue';
 import {Component} from 'vue-property-decorator';
 import FormItem from '@/components/Money/FormItem.vue';
 import Button from '@/components/Button.vue';
-import store from '@/store/index2';
 
 @Component({
   components: {Button, FormItem}
 })
 export default class EditLabel extends Vue {
-  tag = store.findTag(this.$route.params.id);
+  // tag = store.findTag(this.$route.params.id);
+  //TODO
+  tag = {};
 
   created() {
     if (!this.tag) {
@@ -38,17 +39,20 @@ export default class EditLabel extends Vue {
 
   update(name: string) {
     if (this.tag) {
-      store.updateTag(this.tag.id, name);
+      //TODO
+      // store.updateTag(this.tag.id, name);
     }
   }
 
   remove() {
     if (this.tag) {
-      if (store.removeTag(this.tag.id)) {
-        this.$router.back();
-      } else {
-        window.alert('删除失败');
-      }
+      //TODO
+      return;
+      // if (store.removeTag(this.tag.id)) {
+      //   this.$router.back();
+      // } else {
+      //   window.alert('删除失败');
+      // }
     }
   }
 
--- src/views/Money.vue
+++ src/views/Money.vue
@@ -18,28 +18,29 @@ import Types from '@/components/Money/Types.vue';
 import FormItem from '@/components/Money/FormItem.vue';
 import Tags from '@/components/Money/Tags.vue';
 import {Component} from 'vue-property-decorator';
-import oldStore from '@/store/index2';
 
 @Component({
   components: {Tags, FormItem, Types, NumberPad},
   computed: {
     count() {
-      return this.$store.state.count;
+      return this.$store.state.recordList;
     }
   }
 })
 export default class Money extends Vue {
-  recordList = oldStore.recordList;
   record: RecordItem = {
     tags: [], notes: '', type: '-', amount: 0
   };
+  created(){
+    this.$store.commit('fetchRecords')
+  }
 
   onUpdateNotes(value: string) {
     this.record.notes = value;
   }
 
   saveRecord() {
-    oldStore.createRecord(this.record);
+    this.$store.commit('createRecord', this.record);
   }
 }
 </script >
