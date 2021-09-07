9ce42255ba705eee1e0453b5a1c9fb01c24bdabd
你çxuCheng <1390683299@qq.com>
2021-09-07 16:53:42 +0800

    修复一进页面就弹出4个提示的bug

--- src/store/index.ts
+++ src/store/index.ts
@@ -10,6 +10,7 @@ const store = new Vuex.Store({
   state: {
     recordList: [],
     createRecordError: null,
+    createTagError: null,
     tagList: [],
     currentTag: undefined
   } as RootState,
@@ -71,10 +72,11 @@ const store = new Vuex.Store({
       }
     },
     createTag(state, name: string) {
+      state.createTagError = null;
       const names = state.tagList.map(item => item.name);
       if (names.indexOf(name) >= 0) {
-        window.alert('标签名重复了');
-        return 'duplicated';
+        state.createTagError = new Error('tag name duplicated');
+        return;
       }
       const id = createId().toString();
       state.tagList.push({id, name: name});
