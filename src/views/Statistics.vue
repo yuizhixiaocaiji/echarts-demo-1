3737e4b1f9bb9bfac4be4c292a7ebfdc5b4dde6e
你çxuCheng <1390683299@qq.com>
2021-09-07 09:35:20 +0800

    完成列表的css

--- src/views/Statistics.vue
+++ src/views/Statistics.vue
@@ -4,13 +4,46 @@
           :value.sync="type" />
     <Tabs class-prefix="interval" :data-source="intervalList"
           :value.sync="interval" />
-    <div >
-      type: {{ type }}
-      <br />
-      interval: {{ interval }}
-    </div >
+    <ol >
+      <li v-for="(group,index) in result" :key="index" >
+        <h3 class="title" >{{ group.title }}</h3 >
+        <ol >
+          <li v-for="item in group.items" :key="item.id"
+              class="record"
+          >
+            <span >{{ tagString(item.tags) }}</span >
+            <span class="notes" >{{ item.notes }}</span >
+            <span >￥{{ item.amount }}</span >
+          </li >
+        </ol >
+      </li >
+    </ol >
   </Layout >
 </template >
+<style scoped lang="scss" >
+%item {
+  padding: 0 16px;
+  min-height: 40px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+
+.title {
+  @extend %item;
+}
+
+.record {
+  @extend %item;
+  background: white;
+}
+
+.notes {
+  margin-right: auto;
+  margin-left: 16px;
+  color: #999999;
+}
+</style >
 
 <script lang="ts" >
 import Vue from 'vue';
@@ -23,6 +56,33 @@ import recordTypeList from '@/constants/recordTypeList';
   components: {Tabs}
 })
 export default class Statistics extends Vue {
+  // eslint-disable-next-line no-undef
+  tagString(tags: Tag[]) {
+    return tags.length === 0 ? '无' : tags.join(',');
+  }
+
+  get recordList() {
+    // eslint-disable-next-line no-undef
+    return (this.$store.state as RootState).recordList;
+  }
+
+  get result() {
+    const {recordList} = this;
+    type HashtableValue = { title: string, items: RecordList[] }
+    const hashTable: { [key: string]: HashtableValue } = {};
+    for (let i = 0; i < recordList.length; i++) {
+      const [date, time] = recordList[i].createdAt!.split('T');
+      hashTable[date] = hashTable[date] || {title: date, items: []};
+
+      hashTable[date].items.push(recordList[i]);
+    }
+    return hashTable;
+  }
+
+  beforeCreate() {
+    this.$store.commit('fetchRecords');
+  }
+
   type = '-';
   interval = 'day';
   intervalList = intervalList;
