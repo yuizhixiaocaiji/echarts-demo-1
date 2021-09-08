69ac3f2249b1745ac42d78095f8188b5f518a3a4
你çxuCheng <1390683299@qq.com>
2021-09-08 08:33:29 +0800

    修复了404错误

--- src/main.ts
+++ src/main.ts
@@ -20,19 +20,20 @@ new Vue({
 }).$mount('#app');
 
 window.onload = function () {
-  setTimeout(() => {
-    window.scrollTo(0, 10000);
+  setTimeout(function () {
+    window.scrollTo(0, 200);
   }, 0);
 };
 
 if (document.documentElement.clientWidth > 500) {
-  window.alert('请使用手机打开本页面以保证浏览效果');
+  window.alert('请使用手机打开本页面，以保证浏览效果');
   const img = document.createElement('img');
-  img.src = '/qrcode.png';
+  img.src = './qrcode.png';
   img.style.position = 'fixed';
   img.style.left = '50%';
   img.style.top = '50%';
-  img.style.transform = 'translate(-50%, -50%)';
+  img.style.transform = 'translate(-50%,-50%)';
   img.style.boxShadow = '0 0 10px rgba(0,0,0,0.25)';
   document.body.appendChild(img);
 }
+
--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,9 @@
 const path = require('path')
 
+function resolve(dir) {
+  return path.join(__dirname, dir)
+}
+
 module.exports = {
   publicPath: process.env.NODE_ENV === 'production'
     ? '/morney-1-website/'
@@ -7,17 +11,22 @@ module.exports = {
   lintOnSave: false,
   chainWebpack: config => {
     const dir = path.resolve(__dirname, 'src/assets/icons')
+    config.resolve.alias.set('@', resolve('src'));
 
     config.module
       .rule('svg-sprite')
       .test(/\.svg$/)
       .include.add(dir).end() // 包含 icons 目录
       .use('svg-sprite-loader').loader('svg-sprite-loader').options({ extract: false }).end()
-      .use('svgo-loader').loader('svgo-loader').tap(option => ({...option, plugins: [{removeAttrs: {attrs: 'fill'}}]}))
+      .use('svgo-loader').loader('svgo-loader').tap(option => ({
+      ...option,
+      plugins: [ { removeAttrs: { attrs: 'fill' } } ]
+    }))
       .end()
     config.plugin('svg-sprite').use(require('svg-sprite-loader/plugin'), [ { plainSprite: true } ])
     config.module.rule('svg').exclude.add(dir) // 其他 svg loader 排除 icons 目录
-  }
+  },
+
 }
 
 
